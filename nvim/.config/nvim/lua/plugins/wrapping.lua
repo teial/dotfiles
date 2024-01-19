83c4510f53e4e72b4da40dcfc7b3cb7bfca2e174
Teia Leusten <teia.leusten@proton.me>
2024-01-19 15:08:59 +0100

    Add wrapping plugin

---
+++ nvim/.config/nvim/lua/plugins/wrapping.lua
@@ -0,0 +1,25 @@
+return {
+    'andrewferrier/wrapping.nvim',
+    config = function()
+        require('wrapping').setup({
+            create_commands = false,
+            create_keymaps = false,
+            auto_set_mode_filetype_allowlist = {
+                "asciidoc",
+                "gitcommit",
+                "latex",
+                "mail",
+                "markdown",
+                "rst",
+                "tex",
+                "text",
+                "telekasten",
+            },
+            softener = {
+                telekasten = true,
+            },
+        })
+        vim.keymap.set("n", "j", "gj", {})
+        vim.keymap.set("n", "k", "gk", {})
+    end
+}
