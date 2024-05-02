d8dc4d2043d6d1a7dea85cc9a9ff07980e75cb16
Teia Leusten <teia.leusten@proton.me>
2024-05-02 23:02:54 +0300

    Update inlay config and fix identation

--- nvim/.config/nvim/lua/core/inlay.lua
+++ nvim/.config/nvim/lua/core/inlay.lua
@@ -2,16 +2,17 @@ local lsp = vim.lsp
 
 -- Set up inlay hints everywhere by default
 vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-	callback = function(args)
-		local client = lsp.get_client_by_id(args.data.client_id)
-		if client.server_capabilities.inlayHintProvider then
-			lsp.inlay_hint.enable(args.buf, true)
-		end
-	end,
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(args)
+        local client = lsp.get_client_by_id(args.data.client_id)
+        if client.server_capabilities.inlayHintProvider then
+            lsp.inlay_hint.enable(true, { bufnr = args.buf })
+        end
+    end,
 })
 
 -- Easy key to toggle
 vim.keymap.set("n", "<leader>ch", function()
-	lsp.inlay_hint.enable(0, not lsp.inlay_hint.is_enabled())
+    local inlay_hint_enabled = lsp.inlay_hint.is_enabled({ bufnr = 0 })
+    lsp.inlay_hint.enable(not inlay_hint_enabled, { bufnr = 0 })
 end, { desc = "inlay hints" })
