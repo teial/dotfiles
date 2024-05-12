0ad41ad1d902b052a6162d2a783ebd79013ddab8
Teia Leusten <teia.leusten@proton.me>
2024-05-12 17:46:06 +0300

    Remove some comments

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,11 +1,6 @@
 {
   description = "Teia's dotfiles";
-
-  # inputs are other flakes you use within your own flake, dependencies
-  # if you will
   inputs = {
-    # unstable has the 'freshest' packages you will find, even the AUR
-    # doesn't do as good as this, and it's all precompiled.
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -13,8 +8,6 @@
     };
   };
 
-  # In this context, outputs are mostly about getting home-manager what it
-  # needs since it will be the one using the flake
   outputs = { nixpkgs, home-manager, ... }: {
     homeConfigurations = {
       "teial" = home-manager.lib.homeManagerConfiguration {
--- nix/home.nix
+++ nix/home.nix
@@ -1,14 +1,10 @@
 { ... }: {
-  # This is required information for home-manager to do its job
   home = {
     stateVersion = "23.11";
     username = "teial";
     homeDirectory = "/Users/Teia";
-    packages = [ ];
+    packages = [];
   };
   programs.home-manager.enable = true;
-  # I use fish, but bash and zsh work just as well here. This will setup
-  # the shell to use home-manager properly on startup, neat!
-  # programs.zsh.enable = true;
 }
 
