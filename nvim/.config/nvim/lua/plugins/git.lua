1c2e70c84d1702dd1b591a017d34c6c1e359da04
Teia Leusten <teia.leusten@proton.me>
2024-07-05 00:11:16 +0300

    Add neogit plugin

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -64,4 +64,13 @@ return {
             })
         end,
     },
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "sindrets/diffview.nvim",
+            "nvim-telescope/telescope.nvim",
+        },
+        config = true
+    },
 }
