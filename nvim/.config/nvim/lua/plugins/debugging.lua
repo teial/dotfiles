ece235b2539ca5ca8f8b244d98827039512f8808
Teia Leusten <teia.leusten@proton.me>
2024-08-07 12:54:18 +0300

    Update neovim debugging configuration

--- nvim/.config/nvim/lua/plugins/debugging.lua
+++ nvim/.config/nvim/lua/plugins/debugging.lua
@@ -25,7 +25,44 @@ return {
             dapui.close()
         end
 
-        vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, {})
-        vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+        vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "toggle breakpoint" })
+        vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "continue" })
+        vim.keymap.set("n", "<Leader>do", dap.step_over, { desc = "step over" })
+        vim.keymap.set("n", "<Leader>di", dap.step_into, { desc = "step into" })
+
+
+        dap.adapters.lldb = {
+            type = "executable",
+            command = "rust-lldb",
+            name = "lldb",
+        }
+
+        dap.configurations.rust = {
+            {
+                name = "Launch",
+                type = "lldb",
+                request = "launch",
+                program = function()
+                    return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
+                end,
+                cwd = "${workspaceFolder}",
+                stopOnEntry = false,
+            },
+            {
+                name = "Shuttle",
+                type = "lldb",
+                request = "launch",
+                program = "cargo shuttle run",
+                cwd = "${workspaceFolder}",
+                stopOnEntry = false,
+            },
+            {
+                name = 'Attach',
+                type = 'lldb',
+                request = 'attach',
+                processId = require('dap.utils').pick_process,
+                cwd = '${workspaceFolder}',
+            },
+        }
     end,
 }
