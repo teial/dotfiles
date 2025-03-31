c97883d10e4b359c2d1cec6a70ec53d3d463a26c
Teia Leusten <teia.leusten@proton.me>
2025-03-31 20:54:46 +0300

    Add some hardtime to stop relying on hjkl and web

---
+++ nvim/.config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,16 @@
+return {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    lazy = false,
+    keys = {
+        { "j", false },
+        { "k", false },
+        { "<Down>", false },
+        { "<Up>", false },
+        { "j", 'v:count == 0 ? "gj" : "j"', mode = { "n", "x" }, desc = "Down", expr = true, silent = true },
+        { "<Down>", 'v:count == 0 ? "gj" : "j"', mode = { "n", "x" }, desc = "Down", expr = true, silent = true },
+        { "k", 'v:count == 0 ? "gk" : "k"', mode = { "n", "x" }, desc = "Up", expr = true, silent = true },
+        { "<Up>", 'v:count == 0 ? "gk" : "k"', mode = { "n", "x" }, desc = "Up", expr = true, silent = true },
+    },
+    opts = {},
+}
