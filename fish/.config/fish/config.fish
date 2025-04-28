23fac1a0aed8cdf28c04aea71db3cca4c4b6dd55
Teia Leusten <teia.leusten@proton.me>
2025-04-28 04:17:40 +0300

    Improve XDG paths handling in fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,6 +4,15 @@ set fish_color_valid_path
 
 . $fish_path/aliases.fish
 
+# Set XDG paths.
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_STATE_HOME "$HOME/.local/state"
+set -gx XDG_CACHE_HOME "$HOME/.cache"
+set -gx XDG_BIN_HOME "$HOME/.local/bin"
+set -gx XDG_TOOL_HOME "$HOME/.tool" # For tools that don't respect XDG conventions
+fish_add_path -ga "$XDG_BIN_HOME"
+
 if status is-interactive
     # Set up homebrew.
     if test -d /home/linuxbrew/.linuxbrew # Linux
@@ -28,25 +37,30 @@ if status is-interactive
     export GPG_TTY=$(tty)
 
     # Rust setup.
-    fish_add_path -gP "$HOME/.cargo/bin"
+    set -gx RUSTUP_HOME "$XDG_TOOL_HOME/rustup"
+    set -gx CARGO_HOME "$XDG_TOOL_HOME/cargo"
+    fish_add_path -gP "$CARGO_HOME/bin"
 
     # Haskell setup.
-    fish_add_path -gP "$HOME/.ghcup/bin"
+    set -gx GHCUP_USE_XDG_DIRS 1
+    fish_add_path -gP "$GHCUP_INSTALL_BASE_PREFIX//bin"
 
     # Node and yarn paths.
     nvm use latest
     fish_add_path -gP "$HOME/.yarn/bin"
 
-    # Java setup
+    # Java setup.
     fish_add_path -gP /opt/homebrew/opt/openjdk/bin
+
+    # Set up doom emacs.
+    fish_add_path -gP "$XDG_CONFIG_HOME/emacs/bin"
 
     # Set up CLI tools.
     fzf --fish | source
     zoxide init --cmd cd fish | source
 
-    # Set XDG paths.
-    set -gx XDG_CONFIG_HOME "$HOME/.config"
-    set -gx XDG_CACHE_HOME "$HOME/.cache"
+    # Set up XDG compliance for other tools.
+    set -gx LESSHISTFILE "$XDG_STATE_HOME/less/history"
 
     # Additional libraries for compliation.
     set -gx LDFLAGS "-L/opt/homebrew/opt/zlib/lib -L/opt/homebrew/opt/libiconv/lib"
