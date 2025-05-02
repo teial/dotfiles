7cfc1959503c33b23ef3d22491db57ff60fcf5ad
Teia Leusten <teia.leusten@proton.me>
2025-05-02 22:55:02 +0300

    Remove hardtime and add borders to lazy dashboard

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -34,6 +34,9 @@ require("lazy").setup({
         enabled = true, -- check for plugin updates periodically
         notify = false, -- notify on update
     }, -- automatically check for plugin updates
+    ui = {
+        border = "single",
+    },
     performance = {
         rtp = {
             -- disable some rtp plugins
