8218cc10b2d3ce8be547a696877b80b314521855
Teia Leusten <teia.leusten@proton.me>
2025-05-08 20:05:12 +0300

    Force lt to show only 2 levels of subdirs in fish

--- fish/.config/fish/aliases.fish
+++ fish/.config/fish/aliases.fish
@@ -2,6 +2,6 @@
 alias ls="eza --icons=always --color=always --git --header --group-directories-first"
 alias ll="eza -l --icons=always --color=always --git --header --group-directories-first"
 alias la="eza -la --icons=always --color=always --git --header --group-directories-first"
-alias lt="eza --tree --icons=always --color=always --git --header --group-directories-first"
+alias lt="eza --tree --level 2 --icons=always --color=always --git --header --group-directories-first"
 alias cat="bat"
 alias emacs="emacs -nw"
